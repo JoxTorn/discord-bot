72d9e04f3eb4978bcabd5244439c0e2f650f50c4
JoxTorn <joxtorn@gmail.com>
2019-09-22 21:17:20 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const Discord = require("discord.js");
+const fs = require('fs');
+var path = require('path');
+
+//Initating discord client
+const  client = new Discord.Client({forceFetchUsers: true}); 
+//If there is no force fatch users some users will not be fetch and will have problem if i tyr to access then ove guild members list
+//https://github.com/discordjs/discord.js/issues/230
+
+const config = require("./config.json");
+// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
+client.config = config;
+
+//Set bot token 
+client.config.token = process.env.token;
+
+//Set torn api key
+client.config.torn_api_key = process.env.torn_api_key;
+
+//setting export path to go to export directory
+client.config.exportPath = __dirname;
+
+client.commands = {};
+
+//Loading all commands
+fs.readdir("./commands/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./commands/${file}`);
+        let commandName = file.split(".")[0];
+        console.log(`Attempting to load command ${commandName}`);
+        client.commands[commandName] = props;
+    });
+});
+
+//Loading all events
+fs.readdir("./events/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        const event = require(`./events/${file}`);
+        let eventName = file.split(".")[0];
+        console.log(`Attempting to load event ${eventName}`);
+        client.on(eventName, event.bind(null, client));
+    });
+});
+
+//Loading all jobs
+fs.readdir("./jobs/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        const job = require(`./jobs/${file}`);
+        let jobName = file.split(".")[0];
+        console.log(`Attempting to load job ${jobName}`);
+        job.run(client);
+    });
+});
+
+//Log in to discord
+client.login(client.config.token);
---
+++ start.bat
@@ -0,0 +1,4 @@
+set token=DiscordBotTokenGoesHere
+set torn_api_key=YourApiKeyHere
+node index.js
+pause
